e5166fd5a8cb815aad21083888a8bd9ed12f5ee7
Oliver Calder <oliver@calder.dev>
2025-12-05 00:06:15 -0600

    day4: part 2
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -18,12 +18,29 @@ impl Entry {
         }
     }
 
+    fn new_with_roll(is_roll: bool) -> Entry {
+        Entry {
+            is_roll,
+            neighbors: 0,
+        }
+    }
+
     fn set_roll(&mut self) {
         self.is_roll = true;
     }
 
+    fn unset_roll(&mut self) {
+        self.is_roll = false;
+    }
+
     fn inc_neighbors(&mut self) {
         self.neighbors += 1;
+    }
+
+    fn dec_neighbors(&mut self) {
+        self.is_roll;
+        self.neighbors;
+        self.neighbors -= 1;
     }
 
     fn is_movable(&self) -> bool {
@@ -97,7 +114,111 @@ impl RowRememberer {
     }
 }
 
-fn count_movable(r: impl std::io::BufRead) -> usize {
+struct Room {
+    height: usize,
+    width: usize,
+    rows: Vec<Vec<Entry>>,
+}
+
+impl Room {
+    fn from(r: impl std::io::BufRead) -> Room {
+        let rows: Vec<Vec<Entry>> = r
+            .lines()
+            .map_while(Result::ok)
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                line.chars()
+                    .map(|c| Entry::new_with_roll(c == '@'))
+                    .collect::<Vec<Entry>>()
+            })
+            .collect();
+        let height = rows.len();
+        let width = rows.last().unwrap().len();
+        Room {
+            height,
+            width,
+            rows,
+        }
+        .prepare()
+    }
+
+    // This should probably be optimized more...
+    fn find_neighbors(&self, r: usize, c: usize, neighbors: &mut Vec<(usize, usize)>) {
+        neighbors.clear();
+        let n_r = r + 1;
+        let n_c = c + 1;
+        if r > 0 {
+            let p_r = r - 1;
+            if c > 0 {
+                neighbors.push((p_r, c - 1));
+            }
+            neighbors.push((p_r, c));
+            if n_c < self.width {
+                neighbors.push((p_r, n_c));
+            }
+        }
+        if c > 0 {
+            neighbors.push((r, c - 1));
+        }
+        if n_c < self.width {
+            neighbors.push((r, n_c));
+        }
+        if n_r < self.height {
+            if c > 0 {
+                neighbors.push((n_r, c - 1));
+            }
+            neighbors.push((n_r, c));
+            if n_c < self.width {
+                neighbors.push((n_r, n_c));
+            }
+        }
+    }
+
+    /// Assumes all neighbor counts are initially 0. Should only be called when initializing a new
+    /// [Room].
+    fn prepare(mut self) -> Self {
+        let mut neighbors: Vec<(usize, usize)> = Vec::with_capacity(8);
+        for i in 0..self.height {
+            for j in 0..self.width {
+                if !self.rows[i][j].is_roll {
+                    continue;
+                }
+                self.find_neighbors(i, j, &mut neighbors);
+                for (x, y) in &neighbors {
+                    // Probably faster to use checked getter methods rather than pre-check coords
+                    // and then do checked indexing
+                    self.rows[*x][*y].inc_neighbors();
+                }
+            }
+        }
+        self
+    }
+
+    /// Removes any movable rolls, returning the total number which are movable. Rolls are greedily
+    /// removed, so a roll which was not removable at the beginning of the sweep may become movable
+    /// as the result of the removal of previous rolls during the sweep, and thus be itself removed
+    /// during that sweep.
+    fn sweep(&mut self) -> usize {
+        let mut neighbors: Vec<(usize, usize)> = Vec::with_capacity(8);
+        let mut count = 0;
+        for i in 0..self.height {
+            for j in 0..self.width {
+                if !self.rows[i][j].is_movable() {
+                    continue;
+                }
+                count += 1;
+                self.rows[i][j].unset_roll();
+                self.find_neighbors(i, j, &mut neighbors);
+                for (x, y) in &neighbors {
+                    self.rows[*x][*y].dec_neighbors();
+                }
+            }
+        }
+        count
+    }
+}
+
+fn count_initially_movable(r: impl std::io::BufRead) -> usize {
     let mut rememberer = RowRememberer::new();
     let all_but_last: usize = r
         .lines()
@@ -107,9 +228,34 @@ fn count_movable(r: impl std::io::BufRead) -> usize {
     all_but_last + rememberer.tally_prev_row()
 }
 
+fn count_eventually_movable(r: impl std::io::BufRead) -> usize {
+    let mut room = Room::from(r);
+    let mut total_moved = 0;
+    loop {
+        let count = room.sweep();
+        if count == 0 {
+            break;
+        }
+        total_moved += count;
+    }
+    total_moved
+}
+
 fn main() {
-    let result = count_movable(std::io::stdin().lock());
-    println!("Movable rolls: {result}");
+    // Copy stdin out of laziness, we're going to make a full representation anyway...
+    let input: String =
+        std::io::stdin()
+            .lines()
+            .map_while(Result::ok)
+            .fold(String::new(), |mut acc, line| {
+                acc.push_str(&line);
+                acc.push('\n');
+                acc
+            });
+    let initially_movable = count_initially_movable(std::io::BufReader::new(input.as_bytes()));
+    println!("Initially movable rolls: {initially_movable}");
+    let eventually_movable = count_eventually_movable(std::io::BufReader::new(input.as_bytes()));
+    println!("Eventually movable rolls: {eventually_movable}");
 }
 
 #[cfg(test)]
@@ -127,9 +273,105 @@ mod tests {
 @.@.@@@.@.";
 
     #[test]
-    fn test_count_movable() {
+    fn test_count_initially_movable() {
         let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
-        let result = super::count_movable(test_input);
+        let result = super::count_initially_movable(test_input);
         assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_count_eventually_movable() {
+        let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
+        let result = super::count_eventually_movable(test_input);
+        assert_eq!(result, 43);
+    }
+
+    #[test]
+    fn test_find_neighbors() {
+        let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
+        let room = super::Room::from(test_input);
+        for ((i, j), expected) in vec![
+            ((0, 0), vec![(0, 1), (1, 0), (1, 1)]),
+            ((0, 1), vec![(0, 0), (0, 2), (1, 0), (1, 1), (1, 2)]),
+            ((0, 8), vec![(0, 7), (0, 9), (1, 7), (1, 8), (1, 9)]),
+            ((0, 9), vec![(0, 8), (1, 8), (1, 9)]),
+            ((1, 0), vec![(0, 0), (0, 1), (1, 1), (2, 0), (2, 1)]),
+            (
+                (1, 1),
+                vec![
+                    (0, 0),
+                    (0, 1),
+                    (0, 2),
+                    (1, 0),
+                    (1, 2),
+                    (2, 0),
+                    (2, 1),
+                    (2, 2),
+                ],
+            ),
+            (
+                (1, 8),
+                vec![
+                    (0, 7),
+                    (0, 8),
+                    (0, 9),
+                    (1, 7),
+                    (1, 9),
+                    (2, 7),
+                    (2, 8),
+                    (2, 9),
+                ],
+            ),
+            ((1, 9), vec![(0, 8), (0, 9), (1, 8), (2, 8), (2, 9)]),
+            (
+                (5, 5),
+                vec![
+                    (4, 4),
+                    (4, 5),
+                    (4, 6),
+                    (5, 4),
+                    (5, 6),
+                    (6, 4),
+                    (6, 5),
+                    (6, 6),
+                ],
+            ),
+            ((8, 0), vec![(7, 0), (7, 1), (8, 1), (9, 0), (9, 1)]),
+            (
+                (8, 1),
+                vec![
+                    (7, 0),
+                    (7, 1),
+                    (7, 2),
+                    (8, 0),
+                    (8, 2),
+                    (9, 0),
+                    (9, 1),
+                    (9, 2),
+                ],
+            ),
+            (
+                (8, 8),
+                vec![
+                    (7, 7),
+                    (7, 8),
+                    (7, 9),
+                    (8, 7),
+                    (8, 9),
+                    (9, 7),
+                    (9, 8),
+                    (9, 9),
+                ],
+            ),
+            ((8, 9), vec![(7, 8), (7, 9), (8, 8), (9, 8), (9, 9)]),
+            ((9, 0), vec![(8, 0), (8, 1), (9, 1)]),
+            ((9, 1), vec![(8, 0), (8, 1), (8, 2), (9, 0), (9, 2)]),
+            ((9, 8), vec![(8, 7), (8, 8), (8, 9), (9, 7), (9, 9)]),
+            ((9, 9), vec![(8, 8), (8, 9), (9, 8)]),
+        ] {
+            let mut neighbors: Vec<(usize, usize)> = Vec::new();
+            room.find_neighbors(i, j, &mut neighbors);
+            assert_eq!(neighbors, expected);
+        }
     }
 }
