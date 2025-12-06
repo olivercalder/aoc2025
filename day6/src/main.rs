cf485b0745cf15ed2536dad95cbe0e834ebd480b
Oliver Calder <oliver@calder.dev>
2025-12-06 13:33:29 -0600

    day6: part 2
    Signed-off-by: Oliver Calder <oliver@calder.dev>

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
@@ -8,6 +9,7 @@ enum ParseNumsOrOpsError {
     ParseEmpty,
 }
 
+#[derive(Debug)]
 enum Op {
     Add,
     Mul,
@@ -105,9 +107,104 @@ fn vertical_math(r: impl std::io::BufRead) -> impl Iterator<Item = usize> {
         })
 }
 
+struct RawColumn {
+    num: usize,
+    op: Option<Op>,
+}
+
+#[derive(Debug)]
+struct SemanticColumn {
+    nums: Vec<usize>,
+    op: Op,
+}
+
+impl SemanticColumn {
+    fn compute(&self) -> usize {
+        match self.op {
+            Op::Add => self.nums.iter().sum(),
+            Op::Mul => self.nums.iter().product(),
+        }
+    }
+}
+
+/// [GridReader] is an iterator over the [SemanticColumn]s in a grid.
+struct GridReader {
+    width: usize,
+    curr_col: usize,
+    grid: Vec<String>, // for simplicity, split and own
+}
+
+impl GridReader {
+    fn new(r: impl std::io::BufRead) -> Self {
+        let rows: Vec<String> = r
+            .lines()
+            .map_while(Result::ok)
+            .filter(|line| !line.is_empty())
+            .collect();
+        GridReader {
+            width: rows.iter().map(|r| r.len()).max().unwrap(),
+            curr_col: 0,
+            grid: rows,
+        }
+    }
+
+    fn next_raw_column(&mut self) -> Option<RawColumn> {
+        if self.curr_col >= self.width {
+            return None;
+        }
+        let mut pos = self.curr_col;
+        self.curr_col += 1;
+        let mut digits = String::new();
+        let mut op: Option<Op> = None;
+        for row in &self.grid {
+            let Some(c) = row.as_bytes().get(pos) else {
+                continue;
+            };
+            match c {
+                b'0'..=b'9' => digits.push((*c).into()),
+                b'+' => op = Some(Op::Add),
+                b'*' => op = Some(Op::Mul),
+                _ => {} // ignore it
+            }
+        }
+        if digits.is_empty() {
+            return None;
+        }
+        let num: usize = digits.parse().unwrap();
+        Some(RawColumn { num, op })
+    }
+}
+
+impl Iterator for GridReader {
+    type Item = SemanticColumn;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr_col >= self.width {
+            return None;
+        }
+        let mut nums: Vec<usize> = Vec::new();
+        let mut op: Option<Op> = None;
+        while let Some(raw_col) = self.next_raw_column() {
+            nums.push(raw_col.num);
+            op = op.or(raw_col.op);
+        }
+        op.map(|o| SemanticColumn { nums, op: o })
+    }
+}
+
+fn columnar_math(r: impl std::io::BufRead) -> impl Iterator<Item = usize> {
+    let reader = GridReader::new(r);
+    reader.map(|sem_col| sem_col.compute())
+}
+
 fn main() {
-    let result: usize = vertical_math(std::io::stdin().lock()).sum();
-    println!("Sum of vertical computations: {result}");
+    let mut input_buf = Vec::new();
+    std::io::stdin().lock().read_to_end(&mut input_buf).unwrap();
+    let complete_input = String::from_utf8(input_buf).unwrap();
+    let standard: usize = vertical_math(std::io::BufReader::new(complete_input.as_bytes())).sum();
+    println!("Sum of standard computations: {standard}");
+    let columnar: usize = columnar_math(std::io::BufReader::new(complete_input.as_bytes())).sum();
+    println!("Sum of columnar computations: {columnar}");
 }
 
 #[cfg(test)]
@@ -123,5 +220,12 @@ mod tests {
         let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
         let result: Vec<usize> = super::vertical_math(test_input).collect();
         assert_eq!(result, vec![33210, 490, 4243455, 401]);
+    }
+
+    #[test]
+    fn test_columnar_math() {
+        let test_input = std::io::BufReader::new(EXAMPLE_INPUT.as_bytes());
+        let result: Vec<usize> = super::columnar_math(test_input).collect();
+        assert_eq!(result, vec![8544, 625, 3253600, 1058]);
     }
 }
